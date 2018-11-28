4273373d78eec3d1f147d6c8aca6317b055f3cd0
sai1989 <sai.161989@hotmail.com>
2018-11-28 18:32:35 +0530

    login method

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,21 +17,50 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegistrationDto userForRegistration)
         {
-            if(await _repo.UserExists(userForRegistration.UserName.ToLower()))
+            if (await _repo.UserExists(userForRegistration.UserName.ToLower()))
                 return BadRequest("User name already exists");
-            var curUser=new User
+            var curUser = new User
             {
-                UserName=userForRegistration.UserName.ToLower()
+                UserName = userForRegistration.UserName.ToLower()
             };
-            await _repo.Register(curUser,userForRegistration.Password);
+            await _repo.Register(curUser, userForRegistration.Password);
             return StatusCode(201);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+            var claims= new []
+            {
+                //System.Security.Claim
+                new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userFromRepo.UserName)
+            };
+            //Microsoft.IdentityModel.Tokens
+            var key= new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds= new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature); 
+             var tokenDescriptor= new SecurityTokenDescriptor()
+             {
+                 Subject=new ClaimsIdentity(claims),
+                 Expires=System.DateTime.Now.AddDays(1),
+                 SigningCredentials = creds
+             };
+            //System.IdentityModel.Tokens.Jwt
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token=tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            return Ok(new {token=tokenHandler.WriteToken(token)});
         }
     }
 }
